3a611bf3143744fa3b05e56b17950821c20e2595
Bekzodbek <nightcoderr@gmail.com>
2020-07-09 16:47:01 +0500

    sss

--- core/bot.js
+++ core/bot.js
@@ -5,19 +5,29 @@ const composer = new Composer()
 const middleware = (composer) =>
     bot.use(composer.middleware())
 
-if (process.env.ENV.toLowerCase() === "heroku") {
-    bot.launch({
-        webhook: {
-            domain: process.env.URL,
-            hookPath: '/bot',
-            port: process.env.PORT
-        }
-    }).then()
-} else if (process.env.ENV.toLowerCase() === "local") {
-    bot.launch().then()
-} else {
-    console.log("Bot can't be started due to wrong environment!")
-}
+bot.launch({
+    webhook: {
+        domain: 'https://agile-inlet-98836.herokuapp.com/',
+hookPath: '/bot',
+    port: 3000
+}}).then()
+
+
+// if (process.env.ENV.toLowerCase() === "heroku") {
+//     bot.launch({
+//         webhook: {
+//             domain: process.env.URL,
+//             hookPath: '/bot',
+//             port: process.env.PORT
+//         }
+//     }).then()
+// } else if (process.env.ENV.toLowerCase() === "local") {
+//     bot.launch().then()
+// } else {
+//     console.log("Bot can't be started due to wrong environment!")
+// }
+
+
 
 module.exports = { bot , middleware,composer}
 
