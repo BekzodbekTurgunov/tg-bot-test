b302758a68a8504ed74a58de426511f4801429c6
Bekzodbek <nightcoderr@gmail.com>
2020-07-09 16:23:52 +0500

    finall

--- core/bot.js
+++ core/bot.js
@@ -1,17 +1,37 @@
 const { Telegraf,Composer } = require('telegraf')
-const {Composer} = require('micro-bot')
 
-
-// const bot = new Telegraf(process.env.TOKEN)
-const bot =  new Composer()
-
+const bot = new Telegraf(process.env.TOKEN)
 const composer = new Composer()
 const middleware = (composer) =>
     bot.use(composer.middleware())
 
-// bot.launch().then()
+if (process.env.ENV.toLowerCase() === "heroku") {
+    bot.launch({
+        webhook: {
+            domain: process.env.URL,
+            hookPath: '/bot',
+            port: process.env.PORT
+        }
+    }).then()
+} else if (process.env.ENV.toLowerCase() === "local") {
+    bot.launch().then()
+} else {
+    console.log("Bot can't be started due to wrong environment!")
+}
 
 module.exports = { bot , middleware,composer}
+
+
+
+
+
+
+
+
+
+
+
+
 
 //agile-inlet-98836
 //https://agile-inlet-98836.herokuapp.com/ | https://git.heroku.com/agile-inlet-98836.git
